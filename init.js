3e22974e02b505968087bbb4359880adbf9b7081
frhnbaig <68302111+f-baig@users.noreply.github.com>
2024-05-21 21:39:22 -0400

    Update init.js

--- init.js
+++ init.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBo1uyq8UhGwa-SenwM3FFywTxkvUBHwmI",
-  authDomain: "video-call-99634.firebaseapp.com",
-  projectId: "video-call-99634",
-  storageBucket: "video-call-99634.appspot.com",
-  messagingSenderId: "43442346135",
-  appId: "1:43442346135:web:17b5ef93c983ba6850f1d2",
-  measurementId: "G-38ZPFZQDG1"
+  // Text me for the ids and auths
 };
 
 // Initialize Firebase
